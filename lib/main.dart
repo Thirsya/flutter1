310d6846c1816dd257c40845a846730002c161ff
Thirsya <thirsya.widya@gmail.com>
2022-09-24 17:00:32 +0700

    Tugas Layouting

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,7 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
 
   @override
@@ -32,114 +30,48 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView(
-          children: const <Widget>[
-            navbar(),
-            Trending(),
-            Berita(),
-            Berita(),
-          ],
-        ));
-  }
-}
-
-class Berita extends StatelessWidget {
-  const Berita({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(border: Border.all(color: Colors.indigo)),
-      child: Column(children: <Widget>[
-        Container(
-          decoration: BoxDecoration(border: Border.all(color: Colors.indigo)),
-          child: Row(
-            children: <Widget>[
-              Container(
-                width: 170.7,
-                height: 100,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  image: const DecorationImage(
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.fitWidth,
-                    image: AssetImage('assets/images/2.jpg'),
-                  ),
-                ),
-              ),
-              Container(
-                width: 210,
-                height: 100,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat',
-                  textAlign: TextAlign.left,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          alignment: Alignment.centerLeft,
-          child: const Text('Barcelona Feb 13, 2021'),
-        ),
-      ]),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: const <Widget>[
+          navbar(),
+          Trending(),
+          Expanded(child: Scroll()),
+        ],
+      ),
     );
   }
 }
 
-class Trending extends StatelessWidget {
-  const Trending({
+class Scroll extends StatelessWidget {
+  const Scroll({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(border: Border.all(color: Colors.purple)),
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 200,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fitWidth,
-                image: AssetImage('assets/images/1.jpeg'),
+      margin: const EdgeInsets.all(8.0),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  children: const <Widget>[
+                    Berita(),
+                    Berita(),
+                    Berita(),
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.topCenter,
-            child: const Text(
-              'Costa Mendekat Ke Palmeiras',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.centerLeft,
-            color: Colors.purpleAccent,
-            child: const Text(
-              'Transfer',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
@@ -153,17 +85,143 @@ class navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
+      padding: const EdgeInsets.all(3.5),
       margin: const EdgeInsets.all(15),
+      height: 20.5,
+      child: Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: const BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'BERITA TERBARU',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: const BorderSide(
+                  color: Colors.transparent,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyApp()),
+                );
+              },
+              child: const Text(
+                'PERTANDINGAN HARI INI',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Trending extends StatelessWidget {
+  const Trending({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(border: Border.all(color: Colors.purple)),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 150,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                alignment: Alignment.topCenter,
+                fit: BoxFit.fitWidth,
+                image: AssetImage('assets/images/1.jpeg'),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: const Text(
+              "Costa Mendekat ke Palmeiras",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            alignment: Alignment.centerLeft,
+            color: Colors.purpleAccent,
+            child: const Text("Transfer"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Berita extends StatelessWidget {
+  const Berita({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
       child: Column(
         children: <Widget>[
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const <Widget>[
-              Text('BERITA TERBARU'),
-              Text('PERTANDINGAN HARI INI'),
+            children: <Widget>[
+              Container(
+                width: 170.7,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  image: const DecorationImage(
+                    alignment: Alignment.center,
+                    fit: BoxFit.fitWidth,
+                    image: AssetImage('assets/images/2.jpg'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(5.0),
+                  width: 200,
+                  height: 100,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: const Text(
+                      "Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat",
+                      textAlign: TextAlign.left),
+                ),
+              ),
             ],
-          )
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+            ),
+            child: const Text("Barcelona Feb 13, 2021"),
+          ),
         ],
       ),
     );
